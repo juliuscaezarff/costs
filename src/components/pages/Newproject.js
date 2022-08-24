1004710414957481d63e97c8d2dd0eb8ee7d9718
Julius Caezar <julius.caezar25@gmail.com>
2022-08-24 12:12:40 -0400

    add files

--- src/components/pages/Newproject.js
+++ src/components/pages/Newproject.js
@@ -1,14 +1,40 @@
+import { useNavigate } from 'react-router-dom'
+
 import styles from './Newproject.module.css'
 
 import ProjectForm from '../project/ProjectForm'
 
 function NewProject() {
 
+  const history = useNavigate()
+
+  function createPost(project) {
+
+    // initialize cost and services
+    project.cost = 0
+    project.services = []
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(project),
+
+    }).then((resp) => resp.json())
+    .then((data) => {
+      console.log(data)
+      //redirect
+      history('./projects', {message: 'Projeto criado com sucesso!'})
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className={styles.newProjectContainer}>
       <h1>Criar Projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
-     <ProjectForm btnText="Criar projeto"/>
+     <ProjectForm handleSubmit={createPost} btnText="Criar projeto"/>
     </div>
   )
 }
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -7,8 +7,9 @@ import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ handleSubmit, btnText, projectData }) {
   const [categories, setCategories] = useState([]) // eles começam como um array vazio(esperando a resposta da api)
+  const [ project, setProject ] = useState(projectData || {})
 //se não usar esse hook ele vai consultar a api milhões de vezes
   useEffect(() => {
       //fazendo um request (de get) utilizando a "fecth api do js"
@@ -25,24 +26,48 @@ function ProjectForm({ btnText }) {
     .catch(err => console.log(err)) // tratando erro, imprimindo o erro caso haja algo no request
   }, [])
 
+  const submit = (e) => {
+    e.preventDefault()
+    //console.log(project)
+    handleSubmit(project)
+  }
+
+  function handleChange(e) { // reaproveitar em outros forulários
+    setProject({...project, [e.target.name]: e.target.value })
+  }
+
+  function handleCategory(e) { // reaproveitar em outros forulários
+    setProject({...project, category: {
+      id: e.target.value,
+      name: e.target.options[e.target.selectIndex].text,
+     },
+    })
+  }
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type="text"
         text="Nome do projeto"
         name="name"
         placeholder="Insira o nome do projeto"
+        handleOnChange={handleChange}
+        value={project.name ? project.name : ''}
       />
       <Input
         type="number"
         text="Orçamento do projeto"
         name="budget"
         placeholder="Insira o orçamento total"
+        handleOnChange={handleChange}
+        value={project.budget ? project.budget : ''}
       />
       <Select
         name="categoryId"
         text="Selecione a categoria"
         options={categories}
+        handleOnChange={handleCategory}
+        value={project.category ? project.category.id : ''}
       />
       <SubmitButton text={btnText} />
     </form>
