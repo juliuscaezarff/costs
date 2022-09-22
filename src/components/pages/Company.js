f51421ab1b2268153190482af1292e3e529a6487
Julius Caezar <julius.caezar25@gmail.com>
2022-09-22 19:28:30 -0400

    create intro page

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -1,6 +1,23 @@
+import styles from './Company.module.css'
+
+import LinkButton from '../layout/LinkButton'
+
+
 function Company() {
 
-  return <h1>Company</h1>
+  return (
+    <div className={styles.container}>
+      <div className={styles.introContainer}>
+        <h1>Gerencie da melhor maneira seus projetos!</h1>
+        <p>Invista na <span>costs</span> melhore seus projetos em 100%</p>
+        <LinkButton to="/newproject" text="Criar Projeto"/>
+      </div>
+
+      <div>
+        aa
+      </div>
+    </div>
+  )
 }
 
 export default Company
