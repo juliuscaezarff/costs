9304d8c61334311f42a2e476d5bda30e303c644c
Julius Caezar <julius.caezar25@gmail.com>
2022-09-22 20:33:08 -0400

    import icon from react-icon/bs

--- src/components/layout/DiferentialsBox.js
+++ src/components/layout/DiferentialsBox.js
@@ -1,11 +1,14 @@
 import styles from './DiferentialsBox.module.css'
 
+import { BsCheckLg } from 'react-icons/bs'
+
 function DiferentialsBox({text, description}) {
 
   return (
     <div className={styles.box}>
       <h3>{text}</h3>
       <p>{description}</p>
+      <BsCheckLg size={32}/>
     </div>
   )
 }
