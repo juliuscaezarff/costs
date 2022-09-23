357303ca7b9b8dc678f5dd3bf8650836973945e4
Julius Caezar <julius.caezar25@gmail.com>
2022-09-22 21:20:05 -0400

    create description

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -3,24 +3,37 @@ import styles from './Company.module.css'
 import LinkButton from '../layout/LinkButton'
 import DiferentialsBox from '../layout/DiferentialsBox'
 
+import DesignBoard from '../../img/design-board.png'
 
 function Company() {
-
   return (
     <div className={styles.container}>
       <div className={styles.introContainer}>
         <h1>Gerencie da melhor maneira seus projetos!</h1>
-        <p>Invista na <span>costs</span> melhore seus projetos em 100%</p>
-        <LinkButton to="/newproject" text="Criar Projeto"/>
+        <p>
+          Invista na <span>costs</span> melhore seus projetos em 100%
+        </p>
+        <LinkButton to="/newproject" text="Criar Projeto" />
       </div>
 
-      <section className={styles.diferentialsContainer}>
-        <h1>Conquistas da <span>costs</span></h1>
-        <div className={styles.box}>
-        <DiferentialsBox text='+2.100' description='Empresas parceiras'/>
-        <DiferentialsBox text='+11.000' description='Projetos criados'/>
-        <DiferentialsBox text='+5' description='Anos no mercado'/>
+      <section className={styles.conquests}>
+        <h1>
+          Conquistas da <span>costs</span>
+        </h1>
+        <div className={styles.conquestsBox}>
+          <DiferentialsBox text="+2.100" description="Empresas parceiras" />
+          <DiferentialsBox text="+11.000" description="Projetos criados" />
+          <DiferentialsBox text="+5" description="Anos no mercado" />
         </div>
+      </section>
+
+      <section className={styles.description}>
+      <h1>Conte com <span>a gente</span></h1>
+      <img src={DesignBoard} alt="Costs" />
+      <h3>
+        Conte com a nossa plataforma para fazer os seus projetos, e
+        transforma-los em grandes projetos!!
+      </h3>
       </section>
     </div>
   )
